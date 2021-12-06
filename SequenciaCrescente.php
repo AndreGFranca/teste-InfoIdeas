e7c97e3877b9a3a31ab77cc85b0eef2d92976358
AndreFrancaSmartControlPro <andre.franca@smartcontrolpro.com.br>
2021-12-05 23:24:44 -0300

    teste

--- SequenciaCrescente.php
+++ SequenciaCrescente.php
@@ -5,6 +5,7 @@
             echo json_encode($array)." ".json_encode($removed)." true\n";
             return true;
         }
+        for($i = 0;$i < count($array)  ;$i++){            
             for($j = 0;$j < count($array)-1  ;$j++){
                 if($i != $j){
                     if($array[$i] < $array[$i+1]){
